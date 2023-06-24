84658b4f1893583e13cbd3e7bf5cb51990f5b65a
hurjun <hj2002ca@hanmail.net>
2023-06-24 09:41:29 +0545

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,9 @@
+def solution(numbers, direction):
+    answer = []
+    if direction=="right":
+        numbers.insert(0,numbers[-1])
+        numbers.pop()
+    else:
+        numbers.append(numbers[0])
+        del numbers[0]
+    return numbers
