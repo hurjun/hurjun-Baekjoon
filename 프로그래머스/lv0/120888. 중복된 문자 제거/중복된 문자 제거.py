3ceea242a88f73c07dec8be265c7742339ffe6fa
hurjun <hj2002ca@hanmail.net>
2023-07-21 13:45:52 +0545

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    answer = ''
+    my_string=list(my_string)
+    for i in range(len(my_string)):
+        if my_string[i] not in answer:
+            answer+=my_string[i]
+    return answer
