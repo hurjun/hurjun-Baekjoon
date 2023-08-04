256e18a6b203dff98b0abfe730cd0e2f69f1aceb
hurjun <hj2002ca@hanmail.net>
2023-08-04 07:19:00 +0545

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -0,0 +1,8 @@
+def solution(arr, queries):
+    answer = []
+    for i in range(len(queries)):
+        temp=arr[queries[i][0]]
+        arr[queries[i][0]]=arr[queries[i][1]]
+        arr[queries[i][1]]=temp
+        
+    return arr
